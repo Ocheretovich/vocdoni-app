8781c9954abc50e5f4f7d3c04c89a3b7ffbc6b1a
Ocheretovich <ocheretovich@gmail.com>
2026-03-27 12:33:06 +0200

    chore: remove unused horizontal position calculation in FloatingLinkEditor

--- src/components/Editor/plugins/FloatingLinkEditorPlugin.tsx
+++ src/components/Editor/plugins/FloatingLinkEditorPlugin.tsx
@@ -40,26 +40,20 @@ export function setFloatingElemPositionForLinkEditor(
     return
   }
 
-  const floatingElemRect = floatingElem.getBoundingClientRect()
-  const anchorElementRect = anchorElem.getBoundingClientRect()
-  const editorScrollerRect = scrollerElem.getBoundingClientRect()
-
-  let top = targetRect.top - verticalGap
-  let left = targetRect.left - horizontalOffset
-
-  if (top < editorScrollerRect.top) {
+    const floatingElemRect = floatingElem.getBoundingClientRect()
+    const anchorElementRect = anchorElem.getBoundingClientRect()
+    const editorScrollerRect = scrollerElem.getBoundingClientRect()
+  
+    let top = targetRect.top - verticalGap
+  
+    if (top < editorScrollerRect.top) {
     top += floatingElemRect.height + targetRect.height + verticalGap * 2
   }
 
-  if (left + floatingElemRect.width > editorScrollerRect.right) {
-    left = editorScrollerRect.right - floatingElemRect.width - horizontalOffset
-  }
+top -= anchorElementRect.top
 
-  top -= anchorElementRect.top
-  left -= anchorElementRect.left
-
-  floatingElem.style.opacity = '1'
-  floatingElem.style.transform = `translate(-2px, ${top}px)`
+floatingElem.style.opacity = '1'
+floatingElem.style.transform = `translate(-2px, ${top}px)`
 }
 
 function FloatingLinkEditor({

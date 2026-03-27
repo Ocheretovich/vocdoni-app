19bc8dc515d628aabc875bbbbe073007fa64f7a1
Ocheretovich <ocheretovich@gmail.com>
2026-03-27 12:43:11 +0200

    chore: remove unused horizontal position calculation in FloatingLinkEditor

--- src/components/Editor/plugins/FloatingLinkEditorPlugin.tsx
+++ src/components/Editor/plugins/FloatingLinkEditorPlugin.tsx
@@ -45,19 +45,13 @@ export function setFloatingElemPositionForLinkEditor(
   const editorScrollerRect = scrollerElem.getBoundingClientRect()
 
   let top = targetRect.top - verticalGap
-  let left = targetRect.left - horizontalOffset
-
+  
   if (top < editorScrollerRect.top) {
     top += floatingElemRect.height + targetRect.height + verticalGap * 2
   }
-
-  if (left + floatingElemRect.width > editorScrollerRect.right) {
-    left = editorScrollerRect.right - floatingElemRect.width - horizontalOffset
-  }
-
+  
   top -= anchorElementRect.top
-  left -= anchorElementRect.left
-
+  
   floatingElem.style.opacity = '1'
   floatingElem.style.transform = `translate(-2px, ${top}px)`
 }
